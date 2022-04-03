d7116beb1c9ac23ffe768b471914b9f1d9cebc79
Ali Biyabani <alibiyabani@hotmail.com>
2022-04-03 14:02:09 +0430

    admob

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
 import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { rootStore } from './src/redux/store';
-import { StatusBar } from 'react-native';
+import { StatusBar, View, StyleSheet, BackHandler, ToastAndroid } from 'react-native';
+import Netinfo from '@react-native-community/netinfo';
 
 import AppNavigation from './src/AppNavigation'
 import { appTheme } from './src/appTheme';
 
+const Toast = ({ visible, message }) => {
+  if (visible) {
+    ToastAndroid.showWithGravityAndOffset(message, ToastAndroid.CENTER, ToastAndroid.CENTER, 100, 100);
+    return null;
+  }
+  return null;
+};
+
 export default function App() {
+  const [connection, setConnection] = useState(true)
+
+  const netState = async () => {
+    const netinfo = await Netinfo.fetch();
+    setConnection(netinfo.isConnected);
+  }
+  useEffect(() => {
+    netState();
+  }, [])
+
+
   return (
     <Provider store={rootStore}>
       <NavigationContainer>
         <StatusBar backgroundColor={appTheme.colors.primary} barStyle='dark' />
+        <Toast visible={connection} message="Check your Internet Access" />
         <AppNavigation />
       </NavigationContainer>
     </Provider>
   );
 }
+
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -5,8 +5,9 @@ import { appTheme } from '../appTheme'
 import NewsItem from './NewsItem'
 import { Ionicons } from '@expo/vector-icons';
 import { getNews } from '../redux/newsSlice';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AdMobBanner } from 'expo-ads-admob';
 
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const NewsList = () => {
@@ -14,25 +15,27 @@ const NewsList = () => {
     const news = useSelector(state => state.news.newsItems)
     const [newsData, setNewsDate] = useState(news)
     const [refreshing, setRefreshing] = useState(false);
+    const [adMobState, setAdmobeState] = useState(true)
+
     const isLoading = useSelector(state => state.news.isLoading)
     const categoryId = useSelector(state => state.selectedCategoryId.catId)
 
     useEffect(() => {
         dispatch(getNews())
-        getData()
+        //getData()
     }, [dispatch])
 
 
-    const getData = async () => {
-        try {
-            const jsonValue = await AsyncStorage.getItem('@news_List')
-            console.log(JSON.parse(jsonValue))
-            return jsonValue != null ? JSON.parse(jsonValue) : null;
+    // const getData = async () => {
+    //     try {
+    //         const jsonValue = await AsyncStorage.getItem('@news_List')
+    //         console.log(JSON.parse(jsonValue))
+    //         return jsonValue != null ? JSON.parse(jsonValue) : null;
 
-        } catch (e) {
-            // error reading value
-        }
-    }
+    //     } catch (e) {
+    //         // error reading value
+    //     }
+    // }
 
     useEffect(() => {
         if (+categoryId == 0) {
@@ -48,6 +51,10 @@ const NewsList = () => {
         setRefreshing(true)
         dispatch(getNews())
         setRefreshing(false)
+    }
+
+    const hideAdmobe = () => {
+        setAdmobeState(false)
     }
 
     if (isLoading) {
@@ -68,21 +75,42 @@ const NewsList = () => {
 
     if (+categoryId == 0) {
         return (
-            <FlatList
-                data={news}
-                renderItem={({ item, index }) => index > 2 && <NewsItem key={index} content={item} />}
-                keyExtractor={(item, index) => index}
-                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={reloudList} colors={[appTheme.colors.primary]} />
-                }
-            />
+            <View style={styles.container}>
+                <FlatList
+                    data={news}
+                    renderItem={({ item, index }) => index > 2 && <NewsItem key={index} content={item} />}
+                    keyExtractor={(item, index) => index}
+                    refreshControl={<RefreshControl refreshing={refreshing} onRefresh={reloudList} colors={[appTheme.colors.primary]} />
+                    }
+                />
+                <View style={styles.adMob}>
+                    <AdMobBanner
+                        bannerSize="BANNER"
+                        adUnitID="ca-app-pub-7078093402554807/1621765327"
+                        servePersonalizedAds
+                        onDidFailToReceiveAdWithError={hideAdmobe}
+                    />
+                </View>
+            </View>
         )
     }
 
     return (
-        <FlatList
-            data={newsData}
-            renderItem={({ item, index }) => <NewsItem key={index} content={item} />}
-            keyExtractor={(item, index) => index.toString()} />
+        <View style={styles.container}>
+            <FlatList
+                data={newsData}
+                renderItem={({ item, index }) => <NewsItem key={index} content={item} />}
+                keyExtractor={(item, index) => index.toString()} />
+
+            <View style={styles.adMob}>
+                <AdMobBanner
+                    bannerSize="BANNER"
+                    adUnitID="ca-app-pub-7078093402554807/1621765327"
+                    servePersonalizedAds
+                    onDidFailToReceiveAdWithError={hideAdmobe}
+                />
+            </View>
+        </View>
     )
 }
 
@@ -102,6 +130,15 @@ const styles = StyleSheet.create({
         fontSize: 12,
         marginTop: 10,
         fontWeight: 'bold'
-    }
+    },
+    adMob: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: appTheme.colors.background,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: appTheme.colors.background,
+    },
 
 })
